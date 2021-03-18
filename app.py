a943cad43320556bd1af1354eaafb878df3364b5
depp01030@gmail.com <depp01030@gmail.com>
2021-03-18 19:54:24 +0800

    update

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def handle_message(event):
         
         line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        TextSendMessage(text="13131"))
         
         return
     r = '我不想回答耶'
