de7b748756ae794ca3fd379b38dc0fcb143e100a
depp01030@gmail.com <depp01030@gmail.com>
2021-03-18 19:06:04 +0800

    update

--- app.py
+++ app.py
@@ -45,9 +45,18 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    
+    msg = event.message.text
+    r = '我不想回答耶'
+    if msg in ["你好","hi","Hi","妳好"]:
+        r = "我很好啊"
+    elif msg == "你能幹嘛":
+        r = "我現在很笨，什麼都不會"
+    
+               
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
